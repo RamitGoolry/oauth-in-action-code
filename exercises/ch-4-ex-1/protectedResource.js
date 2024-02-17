aa4dae18922033a91919f71bb38675459718f95f
Ramit Goolry <ramitgoolry@gmail.com>
2024-02-17 22:12:26 +0530

    ch4 ex 1 - stilly buggy, debugging

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var bodyParser = require('body-parser');
-var cons = require('consolidate');
-var nosql = require('nosql').load('database.nosql');
-var __ = require('underscore');
-var cors = require('cors');
+let express = require("express");
+let bodyParser = require('body-parser');
+let cons = require('consolidate');
+let nosql = require('nosql').load('database.nosql');
+let __ = require('underscore');
+let cors = require('cors');
 
-var app = express();
+let app = express();
 
 app.use(bodyParser.urlencoded({ extended: true })); // support form-encoded bodies (for bearer tokens)
 
@@ -17,36 +17,69 @@ app.set('json spaces', 4);
 app.use('/', express.static('files/protectedResource'));
 app.use(cors());
 
-var resource = {
-	"name": "Protected Resource",
-	"description": "This data has been protected by OAuth 2.0"
+let resource = {
+  "name": "Protected Resource",
+  "description": "This data has been protected by OAuth 2.0"
 };
 
-var getAccessToken = function(req, res, next) {
-	/*
-	 * Scan for an access token on the incoming request.
-	 */
-	
+let getAccessToken = function(req, res, next) {
+  let inToken = null;
+  let auth = req.headers['authorization'];
+  if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
+    inToken = auth.slice('bearer '.length);
+  } else if (req.body && req.body.access_token) {
+    inToken = req.body.access_token;
+  } else if (req.query && req.query.access_token) {
+    inToken = req.query.access_token;
+  }
+
+  if (!inToken) {
+    res.status(401).end();
+    return;
+  }
+
+  console.log('Incoming token: %s', inToken);
+
+  // NOTE: I find it weird that the protected resource is getting the token from the authorization server's 
+  // database. I would expect the protected resource to have to communicate with the authorization server to 
+  // validate the token and get the authorization scopes. I'm not sure if this is what really happens in 
+  // production systems like Auth0, Okta, etc.
+
+  // Validate the token
+  nosql.one((token) => {
+    if (token.access_token === inToken) {
+      return token;
+    }
+  }, (err, token) => {
+    if (token) {
+      console.log("We found a matching token: %s", inToken);
+    } else {
+      console.log('No matching token was found.');
+    }
+    req.access_token = token;
+    next();
+    return;
+  });
 };
+
+// Protect all routes through middleware that checks for the access token
+app.all('*', getAccessToken);
 
 app.options('/resource', cors());
 
+app.post("/resource", cors(), function(req, res) {
+  if (!req.access_token) {
+    res.status(401).end();
+    return;
+  }
 
-/*
- * Add the getAccessToken function to this handler
- */
-app.post("/resource", cors(), function(req, res){
-
-	/*
-	 * Check to see if the access token was found or not
-	 */
-	
+  res.json(resource);
 });
 
-var server = app.listen(9002, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
+let server = app.listen(9002, 'localhost', function() {
+  let host = server.address().address;
+  let port = server.address().port;
 
   console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
 });
- 
+
